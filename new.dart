42be28ddcdb6c8b31f566a2603f1360683379db5
MUmer539 <107949226+MUmer539@users.noreply.github.com>
2023-03-23 17:56:00 +0500

    basics of dart part 1

---
+++ new.dart
@@ -0,0 +1,3 @@
+void main(){
+    print('Dart with Umer');
+}
---
+++ prog1.dart
@@ -0,0 +1,31 @@
+void main() {
+  ///integer type variable
+  int a = 10;
+  print(a);
+
+  ///string type variable
+  String s = "Dart with Umer";
+  print(s);
+
+  ///double type variable
+  double d = 2.53;
+  print(d);
+
+  ///list type variable
+  List l = [1, 2, 3, 4, 'hello world', 2.1];
+  print(l);
+  print(l[3]);
+
+  /// map type variable
+  Map m = {'name': 'Umer', 'city': 'Lahore'};
+  print(m);
+  print(m['city']);
+
+  ///Dynamic type variable
+  var v = 'Ali';
+  var v2 = 10;
+  var v3 = [10, 20, 30];
+  print(v);
+  print(v2);
+  print(v3);
+}
