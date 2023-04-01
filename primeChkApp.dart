7a22d5f26df5e48340e0e926b6a9bbd2b0788377
MUMER123 <97335781+MUMER123@users.noreply.github.com>
2023-04-01 23:58:24 +0500

    check for prime number

---
+++ primeChkApp.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+void main() {
+ print("\t\t\t***FACTORIAL CALCULATOR***");
+
+ stdout.write('Enter a number: ');
+ int? num = int.parse(stdin.readLineSync()!);
+
+ if(isPrime(num)==true){
+  print('Entered Number is Prime');
+ }
+ else{
+  print('Entered Number is not Prime');
+ }
+
+}
+bool isPrime(int n){
+ int c = 0;
+ for(int i=2;i<n~/2;i++){
+  if(n%i==0){
+   c++;
+  }
+ }
+ if(c==0){
+  return true;
+ }
+ else
+  return false;
+}
